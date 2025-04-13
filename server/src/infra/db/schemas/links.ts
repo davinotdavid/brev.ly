a920a8af6c0e20edbf2e9ef791d85f03d344b8db
Davi Nakano <davinakanoca@gmail.com>
2025-04-13 16:00:03 -0600

    feat: change id strategy to uuidv7 and create DB connection

--- server/src/infra/db/schemas/links.ts
+++ server/src/infra/db/schemas/links.ts
@@ -1,10 +1,10 @@
-import { randomUUID } from "node:crypto";
+import { uuidv7 } from "uuidv7";
 import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
 export const links = pgTable("links", {
   id: text("id")
     .primaryKey()
-    .$defaultFn(() => randomUUID()),
+    .$defaultFn(() => uuidv7()),
   slug: text("slug").notNull().unique(),
   remoteURL: text("remote_url").notNull(),
   accessCount: integer("access_count").default(0),
--- server/src/infra/http/routes/create-link.ts
+++ server/src/infra/http/routes/create-link.ts
@@ -1,7 +1,16 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 
+import { db } from "@/infra/db";
+import { schema } from "@/infra/db/schemas";
+
 export const createLinkRoute: FastifyPluginAsyncZod = async (server) => {
-  server.post("/links", () => {
+  server.post("/links", async () => {
+    // Testing
+    await db.insert(schema.links).values({
+      slug: "aBcD21",
+      remoteURL: "https://www.google.com",
+    });
+
     return "POST to Links!";
   });
 };
