1d271b1024b0bb0dc22a0f6378a5d09727ff37da
Davi Nakano <davinakanoca@gmail.com>
2025-04-14 15:34:08 -0600

    fix: small UI adjustments to not found page

--- web/src/pages/not-found.tsx
+++ web/src/pages/not-found.tsx
@@ -4,8 +4,8 @@ import { NotFoundLogo } from "../components/not-found-logo";
 export function NotFound() {
   return (
     <main className="h-dvh bg-gray-200 flex items-center justify-center">
-      <div className="bg-white rounded-lg p-12 md:p-16 max-w-[580px] text-center mx-3">
-        <NotFoundLogo className="mx-auto w-[164px] md:w-full" />
+      <div className="bg-white rounded-lg p-12 sm:p-16 max-w-[580px] text-center mx-3">
+        <NotFoundLogo className="mx-auto w-[164px] sm:w-full" />
         <p className="my-6 text-xl text-gray-600">Link não encontrado</p>
         <p className="text-md text-gray-500">
           O link que você está tentando acessar não existe, foi removido ou é
